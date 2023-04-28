54712654f1dfe4c8ccec76571cec76f02dfb0059
Ocarinaoftime <botwguardianbotw@gmail.com>
2023-04-28 11:21:12 -0400

    upload

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+shell-interval -t 2 -c "esbuild main.js --bundle > build.js" -e 
---
+++ main.js
@@ -0,0 +1,9 @@
+import kaboom from "./node_modules/kaboom/dist/kaboom.mjs";
+
+
+loadSprite("bean", "assets/Link_LA.png")
+
+add([
+    sprite("bean"),
+    pos(80, 40)
+]);
